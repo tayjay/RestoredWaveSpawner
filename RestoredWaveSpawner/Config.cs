31b63527a57a5ecc3e41e40dde56af68632bda50
tayjay <tay.j.masson@gmail.com>
2025-10-03 14:14:12 -0700

    Combined LabAPI and Exiled dlls

--- RestoredWaveSpawner/Config.cs
+++ RestoredWaveSpawner/Config.cs
@@ -2,19 +2,10 @@
 
 namespace RestoredWaveSpawner
 {
-#if EXILED
     public class Config : Exiled.API.Interfaces.IConfig
-#else
-    public class Config 
-#endif
     {
         
-        
-        
-        
-#if EXILED
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
-#endif
     }
 }
--- RestoredWaveSpawner/Plugin.cs
+++ RestoredWaveSpawner/Plugin.cs
@@ -6,57 +6,93 @@ using LabApi.Loader.Features.Plugins.Enums;
 
 namespace RestoredWaveSpawner
 {
-#if EXILED
-    public class Plugin : Exiled.API.Features.Plugin<Config>
-#else
-    public class Plugin : Plugin<Config>
-#endif
+
+    public class Plugin
     {
-        public static Plugin Instance { get; private set; }
-        public SSSpawnerSettings settings;
 
-#if EXILED
-        public override void OnEnabled()
-#else
-        public override void Enable()
-#endif
+        // Shared constants
+        public static string Name { get; } = "RestoredWaveSpawner";
+        public static string Author { get; } = "TayTay";
+        public static Version Version { get; } = new Version(1, 0, 0, 0);
+
+        // These will be set depending on which plugin system is being used.
+        public static LabAPIPlugin LabAPI { get; private set; } = null;
+        public static ExiledPlugin Exiled { get; private set; } = null;
+
+        /**
+         * Dynamically gets the config from either LabAPI or Exiled, depending on which one is being used.
+         */
+        public static Config Config => LabAPI != null ? LabAPI.Config : Exiled?.Config;
+
+        public static SSSpawnerSettings Settings => LabAPI != null ? LabAPI.settings : Exiled?.settings;
+
+
+        public class LabAPIPlugin : LabApi.Loader.Features.Plugins.Plugin<Config>
         {
 
-            Instance = this;
-            settings = new SSSpawnerSettings();
-            settings.Activate();
+            public static LabAPIPlugin Instance { get; private set; }
+            public SSSpawnerSettings settings;
 
-        }
 
-#if EXILED
-        public override void OnDisabled()
-#else
-        public override void Disable()
-#endif
-        {
-            if (settings != null)
+            public override void Enable()
             {
-                settings.Deactivate();
-                settings = null;
+                Instance = this;
+                LabAPI = this;
+                settings = new SSSpawnerSettings();
+                settings.Activate();
             }
-            Instance = null;
+
+            public override void Disable()
+            {
+                if (settings != null)
+                {
+                    settings.Deactivate();
+                    settings = null;
+                }
+
+                Instance = null;
+                LabAPI = null;
+            }
+
+            public override string Name { get; } = Plugin.Name + ".LabAPI";
+
+            public override string Description { get; } =
+                "A plugin for LabAPI that adds a respawn wave menu back to the game.";
+
+            public override string Author { get; } = Plugin.Author;
+            public override Version Version { get; } = Plugin.Version;
+            public override Version RequiredApiVersion { get; } = new Version(LabApiProperties.CompiledVersion);
         }
 
+        public class ExiledPlugin : Exiled.API.Features.Plugin<Config>
+        {
+            public static ExiledPlugin Instance { get; private set; }
+            public SSSpawnerSettings settings;
 
-        public override string Author { get; } = "TayTay";
-        public override Version Version { get; } = new Version(1, 0, 0, 0);
-        
-#if EXILED
-            public override string Name { get; } = "RestoredWaveSpawner.EXILED";
-            public override PluginPriority Priority { get; } = PluginPriority.Lowest;
-#else
-        public override string Name { get; } = "RestoredWaveSpawner.LabAPI";
+            public override void OnEnabled()
+            {
+                Instance = this;
+                Exiled = this;
+                settings = new SSSpawnerSettings();
+                settings.Activate();
+            }
 
-        public override string Description { get; } =
-            "Readds the wave spawner option for SCP:SL until it's officially added back into the game.";
+            public override void OnDisabled()
+            {
+                if (settings != null)
+                {
+                    settings.Deactivate();
+                    settings = null;
+                }
 
-        public override Version RequiredApiVersion { get; } = new Version(LabApiProperties.CompiledVersion);
-        public override LoadPriority Priority { get; } = LoadPriority.Lowest;
-#endif
+                Instance = null;
+                Exiled = null;
+            }
+
+            public override string Name { get; } = $"{Plugin.Name}.Exiled";
+            public override string Author { get; } = Plugin.Author;
+            public override Version Version { get; } = Plugin.Version;
+            public override PluginPriority Priority { get; } = PluginPriority.High;
+        }
     }
 }
